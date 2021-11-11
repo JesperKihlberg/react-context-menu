9cd292945ce440b656f2ccda396a18eebd25405a
Jesper Kihlberg <jesper.kihlberg@gmail.com>
2021-11-11 11:01:28 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { AppBar, Avatar, Button, createStyles, Icon, IconButton, makeStyles, SvgIcon, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    imageIcon: {
+      display: "flex",
+      height: "inherit",
+      width: "inherit",
+    },
+    iconRoot: {
+      textAlign: "center",
+    },
+    questionContainers: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+  })
+);
+
+const App: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Avatar aria-label="recipe" style={{ backgroundColor: "transparent" }}>
+              <img className={classes.imageIcon} src="./favicon.svg" />
+            </Avatar>
+            <Button color="inherit">react-context-menu</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </div>
+  );
+};
+
+export default App;
